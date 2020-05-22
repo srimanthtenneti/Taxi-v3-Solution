e1905ddebbc0d54178a81c062c406788b973fb82
Srimanth Tenneti <40752683+srimanthtenneti@users.noreply.github.com>
2020-05-22 14:01:44 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -24,17 +24,17 @@ class Agent:
         self.Q = defaultdict(lambda: np.zeros(self.nA))
 
     def select_action(self, state, eps,env):
-        if(random.random() > eps):
+        if(random.random() > eps):     # Epsilon greedy policy definition
             return (np.argmax(self.Q[state]))
         else:
             return (random.choice(np.arange(env.action_space.n))) 
         
     def update_Q_sarsa(self , alpha ,gamma ,  Q , state , action , reward , next_state = None):
             current = self.Q[state][action]
-            Qsa_next = np.max(Q[next_state]) if next_state is not None else 0
+            Qsa_next = np.max(Q[next_state]) if next_state is not None else 0 # Q-Learning
             target = reward + (gamma * Qsa_next)
             new_value = current + (alpha * (target - current))
             return new_value
 
     def step(self, alpha  , gamma , state, action, reward, next_state, done):
-        self.Q[state][action]  += self.update_Q_sarsa(self , alpha , gamma , self.Q  ,state , action , reward , next_state)
+        self.Q[state][action]  += self.update_Q_sarsa(self , alpha , gamma , self.Q  ,state , action , reward , next_state) # Update 
